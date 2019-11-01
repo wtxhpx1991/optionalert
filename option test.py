78dd52dd195dcff9402b6f41c6952026a6c8b998
wtxhpx1991 <wtxhpx1991@126.com>
2019-11-01 08:41:29 +0800

    my first change

--- option test.py
+++ option test.py
@@ -7,3 +7,10 @@ w.start()
 OptionContractRawData = w.wset("optioncontractbasicinfo", "exchange=sse;windcode=510050.SH;status=trading")
 OptionContractData = pd.DataFrame(OptionContractRawData.Data).T
 OptionContractData.columns = OptionContractRawData.Fields
+OptionContractName = OptionContractData.wind_code.map(lambda x: str(x) + ".SH")
+# 生成期权合约代码
+OptionContractNameCode = ",".join(OptionContractName)
+# 提取分钟级历史数据
+aa = w.wsi(OptionContractNameCode, "open,high,low,close,volume,amt,chg,pct_chg,oi,begintime,endtime",
+           "2019-10-30 09:00:00", "2019-10-31 18:25:00", "PriceAdj=F")
+
