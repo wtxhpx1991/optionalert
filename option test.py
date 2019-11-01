974644379f833bf60ee522ed3100745901cbe5a1
wtxhpx1991 <wtxhpx1991@126.com>
2019-11-01 08:24:53 +0800

    my first change

--- option test.py
+++ option test.py
@@ -5,6 +5,5 @@ w.start()
 
 # 获取期权合约信息OptionContractRawData
 OptionContractRawData = w.wset("optioncontractbasicinfo", "exchange=sse;windcode=510050.SH;status=trading")
-OptionContractData=OptionContractRawData.Data
-
-pd.DataFrame(OptionContractData)
+OptionContractData = pd.DataFrame(OptionContractRawData.Data).T
+OptionContractData.columns = OptionContractRawData.Fields
