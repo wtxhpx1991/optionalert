d5f15a72ee95030036b900502594f637aadb171a
wtxhpx1991 <wtxhpx1991@126.com>
2019-11-01 16:37:03 +0800

    my first change

--- option test.py
+++ option test.py
@@ -1,11 +1,39 @@
 from WindPy import *
 import pandas as pd
+import numpy as np
+from scipy.stats import norm
+
 import datetime as dt
 
 w.start()
 # 定义无风险利率以及股息率
-RISK_FREE_RATE = 0.025
+RISK_FREE_INTEREST_RATE = 0.025
 DIVIDEND_RATE = 0
+
+
+# 欧式认购期权价格计算
+def EuropeanCallPrice(UnderlyingPrice, ExercisePrice, Time, InterestRate, DividendRate, Volatility):
+    dt = Volatility * (Time ** 0.5)
+    d1 = np.log(UnderlyingPrice / ExercisePrice) + (InterestRate - DividendRate + 0.5 * (Volatility ** 2)) / dt
+    d2 = d1 - dt
+    nd1 = norm.cdf(d1)
+    nd2 = norm.cdf(d2)
+    result = np.exp(-DividendRate * Time) * UnderlyingPrice * nd1 - ExercisePrice * np.exp(-InterestRate * Time) * nd2
+    return result
+
+
+# 欧式认沽期权价格计算
+def EuropeanPutPrice(UnderlyingPrice, ExercisePrice, Time, InterestRate, DividendRate, Volatility):
+    dt = Volatility * (Time ** 0.5)
+    d1 = np.log(UnderlyingPrice / ExercisePrice) + (InterestRate - DividendRate + 0.5 * (Volatility ** 2)) / dt
+    d2 = d1 - dt
+    nd1 = norm.cdf(-d1)
+    nd2 = norm.cdf(-d2)
+    result = ExercisePrice * np.exp(-InterestRate * Time) * nd2 - np.exp(-DividendRate * Time) * UnderlyingPrice * nd1
+    return result
+
+
+
 # 获取期权合约信息OptionContractRawData
 OptionContractNameRawData = w.wset("optioncontractbasicinfo", "exchange=sse;windcode=510050.SH;status=trading")
 OptionContractNameData = pd.DataFrame(OptionContractNameRawData.Data).T
@@ -26,9 +54,9 @@ OptionContractMinuteData['recodetime'] = OptionContractMinuteData['datetime'].ma
 # 生成分析数据
 OptionContractData = pd.merge(OptionContractMinuteData, OptionContractNameData, left_on="windcode",
                               right_on="wind_code", how="left")
-# 检验数据是否正确
-OptionContractDataTest = OptionContractData.groupby(by=["limit_month", "exercise_price", "call_or_put"]).size()
-# 每个合约数据相同
-OptionContractDataTest.min() == OptionContractDataTest.max()
-# 每个合约都有认沽认购
-OptionContractDataTest.count(level='call_or_put')[0] == OptionContractDataTest.count(level='call_or_put')[1]
+# # 检验数据是否正确
+# OptionContractDataTest = OptionContractData.groupby(by=["limit_month", "exercise_price", "call_or_put"]).size()
+# # 每个合约数据相同
+# OptionContractDataTest.min() == OptionContractDataTest.max()
+# # 每个合约都有认沽认购
+# OptionContractDataTest.count(level='call_or_put')[0] == OptionContractDataTest.count(level='call_or_put')[1]
