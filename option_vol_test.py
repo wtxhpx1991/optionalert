ba320fc52e67360e845c40394d3926240c335503
wtxhpt1991 <wtxhpx1991@126.com>
2019-12-04 22:57:03 +0800

    use bisect package

--- option_vol_test.py
+++ option_vol_test.py
@@ -3,6 +3,7 @@
 
 import option
 import pandas as pd
+import bisect
 from WindPy import *
 
 InterestRate = 0.025
@@ -52,7 +53,8 @@ UnderlyingTempData['datetime'] = UnderlyingRawData.Times
 OptionContractData = pd.merge(OptionContractData, UnderlyingTempData, on='datetime', how='left')
 # 计算到期日
 OptionContractData['datetimef'] = OptionContractData['datetime'].map(lambda x: x.strftime('%Y-%m-%d'))
-
+OptionContractData['exercise_datef'] = OptionContractData['exercise_date'].map(
+    lambda x: datetime.strptime(x, '%Y-%m-%d').date())
 # def dayscount(arrlike, startdate, enddate):
 #     return w.tdayscount(arrlike[startdate], arrlike[enddate], "").Data[0][0]
 # aa = OptionContractData.head(100)
@@ -61,10 +63,34 @@ OptionContractData['datetimef'] = OptionContractData['datetime'].map(lambda x: x
 #          StartDate="datetimef",
 #          EndDate="exercise_date")
 # OptionContractData=OptionContractData.head(100)
-OptionContractData["time"] = OptionContractData.apply(option.TradeCalendar.TradeDaysCountAnnualizedForApply, axis=1,
-                                                      StartDate="datetimef",
-                                                      EndDate="exercise_date")
-OptionContractData["time"] = OptionContractData["time"] + 1 / 252
+
+
+# 获取交易日历
+TradeCalendar = option.TradeCalendar.TradeCalendarStartToEnd(START_DATE, '2020-10-10')
+OptionContractData_startdate = pd.DataFrame(OptionContractData['datetime'].drop_duplicates())
+
+
+def startdateindex(arrlike, i):
+    return bisect.bisect_left(TradeCalendar, arrlike[i])
+
+
+OptionContractData_startdate['datetime_index'] = OptionContractData_startdate.apply(startdateindex, axis=1,
+                                                                                    i="datetime")
+
+
+def enddateindex(arrlike, i):
+    return bisect.bisect_right(TradeCalendar, arrlike[i])
+
+
+OptionContractData_enddate = pd.DataFrame(OptionContractData['exercise_datef'].drop_duplicates())
+OptionContractData_enddate['exercise_datef_index'] = OptionContractData_enddate.apply(enddateindex, axis=1,
+                                                                                      i="exercise_datef")
+
+OptionContractData = pd.merge(OptionContractData, OptionContractData_startdate, on='datetime', how='left')
+OptionContractData = pd.merge(OptionContractData, OptionContractData_enddate, on='exercise_datef', how='left')
+OptionContractData['time'] = (OptionContractData['exercise_datef_index'] - OptionContractData['datetime_index']) / 252
+OptionContractData["InterestRate"] = InterestRate
+OptionContractData["DividendRate"] = DividendRate
 OptionContractData['volatility'] = OptionContractData.apply(option.OptionGreeksMethod.ImpliedVolatilityForApply, axis=1,
                                                             Direction="call_or_put",
                                                             UnderlyingPrice="PRE_CLOSE",
@@ -78,4 +104,9 @@ OptionContractData['delta'] = OptionContractData.apply(option.OptionGreeksMethod
                                                        ExercisePrice="exercise_price", Time="time",
                                                        InterestRate="InterestRate",
                                                        DividendRate="DividendRate",
-                                                       Volatility="Volatility")
+                                                       Volatility="volatility")
+
+
+OptionContractData.plot('delta','vol3minutes',kind='scatter')
+OptionContractData.plot('time','vol3minutes',kind='scatter')
+OptionContractData.plot('time','vol3minutes',kind='scatter')
